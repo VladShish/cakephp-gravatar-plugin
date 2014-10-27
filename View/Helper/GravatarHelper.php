dce5a90649ccb435f4d1cd9223ccc87e38078208
Vlad <vladshish89@gmail.com>
2014-10-27 19:41:28 +0200

    fix style

--- View/Helper/GravatarHelper.php
+++ View/Helper/GravatarHelper.php
@@ -49,13 +49,7 @@ class GravatarHelper extends AppHelper {
 		$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
 		$headers = @get_headers($uri);
 
-		if (!preg_match('|200|', $headers[0])) {
-			$has_valid_avatar = false;
-		} else {
-			$has_valid_avatar = true;
-		}
-
-		return $has_valid_avatar;
+		return preg_match('|200|', $headers[0]) ? true: false;
 	}
 
 }
